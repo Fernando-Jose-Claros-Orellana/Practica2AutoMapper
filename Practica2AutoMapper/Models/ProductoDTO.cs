4a4cbd2ab490920ad9f068ff0f469f0f74ac88ab
Fernando-Jose-Claros-Orellana <ferinocente357@gmail.com>
2024-10-09 10:57:42 -0600

    Agregar archivos de proyecto.

---
+++ Practica2AutoMapper/Models/ProductoDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace Practica2AutoMapper.Models
+{
+    public class ProductoDTO
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public decimal Precio { get; set; }
+    }
+}
---
+++ Practica2AutoMapper/PerfilDeMapeo.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using Practica2AutoMapper.Models;
+
+namespace Practica2AutoMapper
+{
+    public class PerfilDeMapeo : Profile
+    {
+        public PerfilDeMapeo()
+        {
+            CreateMap<Producto, ProductoDTO>();
+        }
+    }
+}
